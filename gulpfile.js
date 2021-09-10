747158a8922f423c1cf6fdf4668f418243bd0d68
xencodes <xencodes@icloud.com>
2021-09-10 19:36:07 +0530

    chore: add gulp and update gitignore

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp')
+const run = require('gulp-run')
+
+gulp.task('dashboard', (cb) => {
+	run('cd packages/dashboard && yarn build').exec('', () => {
+		cb()
+	})
+})
+
+gulp.task('dist', (cb) => {
+	gulp
+		.src('packages/dashboard/build/**')
+		.pipe(gulp.dest('packages/synckage/src/web/webapp'))
+	cb()
+})
