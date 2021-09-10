02827a6356cd991c0782912762e8d7ee958550ec
xencodes <xencodes@icloud.com>
2021-09-10 19:20:25 +0530

    chore: add scripts, dependecies and logs components

--- packages/dashboard/src/App/components/Project.js
+++ packages/dashboard/src/App/components/Project.js
@@ -1,28 +1,32 @@
 import {
 	Box,
-	Center,
+	Code,
+	Divider,
 	Flex,
 	GridItem,
 	Heading,
+	HStack,
 	SimpleGrid,
+	Spacer,
 	Stack,
 	Text,
 } from '@chakra-ui/layout'
-import { Stat, StatLabel, StatNumber, StatHelpText } from '@chakra-ui/react'
+import { Stat, StatLabel, StatNumber, Button } from '@chakra-ui/react'
+import { ScrollBarCSS } from '../../constants/scrollbar'
 import { projectType } from '../utils/project-type'
 
 const Project = ({ location }) => {
 	const { state: project } = location
 
-	const { name, description, dependencies, license } = project
+	const { name, description, dependencies, license, scripts } = project
 	const allDeps = Object.keys(dependencies)
+	const projectTypes = projectType(allDeps)
 
 	const Stats = () => {
 		const depCount = allDeps.length
-		const types = projectType(allDeps)
 
 		return (
-			<SimpleGrid columns={3} spacing={4}>
+			<SimpleGrid columns={{ base: 1, md: 3 }} spacing={4} maxW='4xl'>
 				{/*  Total Dependencies */}
 				<GridItem colSpan={1}>
 					<Stat shadow='md' rounded='lg' p={4}>
@@ -41,29 +45,101 @@ const Project = ({ location }) => {
 					</Stat>
 				</GridItem>
 
-				{/*  Project Type */}
-				<Flex shadow='md' rounded='lg' p={4}>
-					<Center>
-						{types.map(({ icon }, idx) => (
-							<Box key={idx} mr={2} fontSize='6xl'>
-								{icon}
-							</Box>
-						))}
-					</Center>
-				</Flex>
+				{/*  Project Status */}
+				<GridItem colSpan={1}>
+					<Stat shadow='md' rounded='lg' p={4}>
+						<StatNumber fontSize='4xl'>{'Stopped'}</StatNumber>
+						<StatLabel fontSize='xl'>Status</StatLabel>
+					</Stat>
+				</GridItem>
 			</SimpleGrid>
+		)
+	}
+
+	const Logs = () => {
+		return (
+			<Stack borderWidth='1px' rounded='lg' p={2}>
+				<Heading fontWeight='normal'>Logs</Heading>
+				<Box h='300px' overflow='auto'>
+					<p>Nostrud ex irure occaecat id.</p>
+				</Box>
+			</Stack>
+		)
+	}
+
+	const Scripts = () => {
+		const allScripts = Object.keys(scripts)
+		return (
+			<Box borderWidth='1px' rounded='lg' p={2}>
+				<Heading fontWeight='normal'>Scripts</Heading>
+				<Stack mt={4} p={2} h='64' overflow='auto' css={ScrollBarCSS}>
+					{allScripts.map((script) => (
+						<Box>
+							<Flex>
+								<Box>
+									<Text fontWeight='semibold'>{script}</Text>
+									<Code>{scripts[script]}</Code>
+								</Box>
+								<Spacer />
+								<Button size='sm'>{script}</Button>
+							</Flex>
+						</Box>
+					))}
+				</Stack>
+			</Box>
+		)
+	}
+
+	const Dependencies = () => {
+		return (
+			<Box borderWidth='1px' rounded='lg' p={2}>
+				<Heading fontWeight='normal'>Dependencies</Heading>
+				<Stack mt={4} p={2} h='64' overflow='auto' css={ScrollBarCSS}>
+					{allDeps.map((dep) => (
+						<Box>
+							<Flex>
+								<Box>
+									<Text fontWeight='semibold'>{dep}</Text>
+									<Code>{dependencies[dep]}</Code>
+								</Box>
+								<Spacer />
+								<Button size='sm'>remove</Button>
+							</Flex>
+						</Box>
+					))}
+				</Stack>
+			</Box>
 		)
 	}
 
 	return (
 		<Box>
 			<Stack bg='gray.700' p={4} textColor='gray.300'>
-				<Heading>{name}</Heading>
-				<Text>{description}</Text>
+				<Flex>
+					<Stack>
+						<Heading>{name}</Heading>
+						<Text>{description}</Text>
+					</Stack>
+					<Spacer />
+					<HStack>
+						{projectTypes.map((type) => (
+							<Box fontSize='4xl'>{type.icon}</Box>
+						))}
+					</HStack>
+				</Flex>
 			</Stack>
-			<Box p={4}>
+			<Stack p={4} spacing={6}>
 				<Stats />
-			</Box>
+				<SimpleGrid columns={10} spacing={4}>
+					<GridItem colSpan={{ base: 10, md: 4 }}>
+						<Scripts />
+					</GridItem>
+					<GridItem colSpan={{ base: 10, md: 6 }}>
+						<Dependencies />
+					</GridItem>
+				</SimpleGrid>
+				<Logs />
+			</Stack>
 		</Box>
 	)
 }
