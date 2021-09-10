bb751545c866ce7f34f7473375208ab627823a29
xencodes <xencodes@icloud.com>
2021-09-10 19:35:20 +0530

    chore: add webapp directory

--- packages/synckage/src/web/server.js
+++ packages/synckage/src/web/server.js
@@ -1,4 +1,5 @@
 const PORT = 9999
+const path = require('path')
 const feathers = require('@feathersjs/feathers')
 const express = require('@feathersjs/express')
 const socketio = require('@feathersjs/socketio')
@@ -7,10 +8,12 @@ const socketio = require('@feathersjs/socketio')
 const Common = require('./services/common')
 const Projects = require('./services/projects')
 
+const webappDir = path.join(__dirname, 'webapp')
+
 const app = express(feathers())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(express.static(__dirname))
+app.use(express.static(webappDir))
 
 app.configure(express.rest())
 app.configure(socketio())
@@ -23,6 +26,10 @@ app.publish(() => app.channel('synckage'))
 // setup services
 app.use('/common', new Common())
 app.use('/projects', new Projects())
+
+app.get('*', (req, res) => {
+	res.sendFile(webappDir + '/index.html')
+})
 
 module.exports = () => {
 	app.listen(PORT, () => console.log(`server listening on ${PORT}`.green.bold))
