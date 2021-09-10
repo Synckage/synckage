00af31fd9f526f1fca74f3ea07575c8abfa069de
xencodes <xencodes@icloud.com>
2021-09-10 19:18:33 +0530

    style: custom scrollbar css

---
+++ packages/dashboard/src/constants/scrollbar.js
@@ -0,0 +1,12 @@
+export const ScrollBarCSS = {
+	'&::-webkit-scrollbar': {
+		width: '4px',
+	},
+	'&::-webkit-scrollbar-track': {
+		width: '6px',
+	},
+	'&::-webkit-scrollbar-thumb': {
+		background: '#2E3648',
+		borderRadius: '24px',
+	},
+}
