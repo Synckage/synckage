87cbeb6e504bd1501a5d04f564c35871542fdfe3
xencodes <xencodes@icloud.com>
2021-09-11 12:41:52 +0530

    fix: update cors and allowEI03

--- packages/synckage/src/web/server.js
+++ packages/synckage/src/web/server.js
@@ -2,9 +2,17 @@ const Projects = require('./services/projects')
 
 const app = require('express')()
 const http = require('http').Server(app)
-const io = require('socket.io')(http)
+const io = require('socket.io')(http, {
+	allowEIO3: true,
+	cors: {
+		origin: 'http://localhost:3000',
+		methods: ['GET', 'POST'],
+		credentials: true,
+	},
+})
 
 io.on('connection', async (socket) => {
+	console.log('socket', socket.id)
 	socket.emit('projects', await Projects.find())
 	socket.on('disconnect', function () {
 		console.log('A user disconnected')
