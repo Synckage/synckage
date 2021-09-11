69ea97527367afb02e28e87b010507b7c0129ed8
xencodes <xencodes@icloud.com>
2021-09-11 16:57:20 +0530

    chore: nothing

--- packages/synckage/src/web/server.js
+++ packages/synckage/src/web/server.js
@@ -5,6 +5,7 @@ const Projects = require('./services/projects')
 const express = require('express')
 const app = express()
 const http = require('http').Server(app)
+
 const io = require('socket.io')(http, {
 	allowEIO3: true,
 	cors: {
